f885d6455e02afb20b7af5a12954319d6592250d
Miller Solis <msolissa@sfu.ca>
2020-11-04 01:53:54 -0800

    Create SignIn.java

---
+++ Development/app/src/main/java/com/cmpt275/house/classDef/SignIn.java
@@ -0,0 +1,25 @@
+package com.cmpt275.house.classDef;
+
+import com.cmpt275.house.interfaceDef.signIn;
+
+class SignIn implements signIn {
+    @Override
+    public void createAccount(String email, String displayName, String password) {
+
+    }
+
+    @Override
+    public void forgotPassword(String email) {
+
+    }
+
+    @Override
+    public void signInUser(String email, String password) {
+
+    }
+
+    @Override
+    public boolean isUserSignedIn() {
+        return false;
+    }
+}
