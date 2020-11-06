30e87e36c80dbc182d716afcbaace575030c79d9
rstolys <44356584+rstolys@users.noreply.github.com>
2020-11-06 10:32:28 -0800

    Updates  to classes and interfaces
    Both for front and backend

--- Development/app/src/main/java/com/cmpt275/house/interfaceDef/settingsCallbacks.java
+++ Development/app/src/main/java/com/cmpt275/house/interfaceDef/settingsCallbacks.java
@@ -5,6 +5,6 @@ import com.cmpt275.house.classDef.userInfo;
 
 public interface settingsCallbacks {
     public void onSettingsStringReturn(String str, String functionName);
-    public void onUserInfoReturn(userInfo uInfos, String functionName);
+    public void onUserInfoReturn(userInfo uInfo, String functionName);
     public void onSettingsBooleanReturn(boolean result, String functionName);
 }
--- Development/app/src/main/java/com/cmpt275/house/interfaceDef/userCallbacks.java
+++ Development/app/src/main/java/com/cmpt275/house/interfaceDef/userCallbacks.java
@@ -3,7 +3,7 @@ package com.cmpt275.house.interfaceDef;
 import com.cmpt275.house.classDef.userInfo;
 
 public interface userCallbacks {
-    public void onUserInfoArrayReturn(userInfo uInfo, String functionName);
+    public void onUserInfoArrayReturn(userInfo[] uInfos, String functionName);
     public void onUserInfoReturn(userInfo uInfo, String functionName);
     public void onUserBooleanReturn(userInfo uInfo, String functionName);
 }
